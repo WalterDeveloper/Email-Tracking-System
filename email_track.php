d345ebaed538e1073206b7f411048b0289540c8f
Walter Developer <47099208+WalterDeveloper@users.noreply.github.com>
2024-12-10 10:00:10 +0200

    Update email_track script

--- email_track.php
+++ email_track.php
@@ -6,7 +6,5 @@ $connect = new PDO("mysql:host=localhost;dbname=email_track_database", "root",""
 	INSERT INTO  email_track (email_track_code,email_status,email_open_datetime) VALUES (?,?,?) ";
 	$statement = $connect->prepare($query);
 	$statement->execute([''.$_GET["code"].'','yes',''.date("Y-m-d H:i:s", STRTOTIME(date('h:i:sa'))).'']);
-
 }
-
 ?>
