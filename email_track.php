3c413475a146485458bba4927220ae37ac9525ab
Walter Developer <47099208+WalterDeveloper@users.noreply.github.com>
2025-01-22 12:55:51 +0200

    Update email_track.php

--- email_track.php
+++ email_track.php
@@ -2,6 +2,7 @@
 if (isset($_GET["code"]) && !empty($_GET["code"]))
 {
 $connect = new PDO("mysql:host=localhost;dbname=email_track_database", "root","");
+	
 	$query = "
 	INSERT INTO  email_track (email_track_code,email_status,email_open_datetime) VALUES (?,?,?) ";
 	$statement = $connect->prepare($query);
